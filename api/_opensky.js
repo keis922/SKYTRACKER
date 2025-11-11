732074d1e0bffd60b4a88b8d8903fc6a916b63f5
keis922 <aissaouikeis@gmail.com>
2025-11-11 13:59:52 +0100

    Historique api/_opensky.js depuis _opensky.v2.js

--- api/_opensky.js
+++ api/_opensky.js
@@ -10,7 +10,17 @@ import axios from "axios";
 
 const openSkyStatesUrl = "https://opensky-network.org/api/states/all";
 
+function buildHeaders() {
+  if (process.env.OPENSKY_USERNAME && process.env.OPENSKY_PASSWORD) {
+    const auth = Buffer.from(
+      `${process.env.OPENSKY_USERNAME}:${process.env.OPENSKY_PASSWORD}`
+    ).toString("base64");
+    return { Authorization: `Basic ${auth}` };
+  }
+  return {};
+}
+
 export async function fetchOpenSkyStates() {
-  const { data } = await axios.get(openSkyStatesUrl);
+  const { data } = await axios.get(openSkyStatesUrl, { headers: buildHeaders() });
   return data;
 }
