467209df5d684a19792e2eaaded3daa7a4e5dd41
keis922 <aissaouikeis@gmail.com>
2025-11-11 10:52:56 +0100

    Historique backend/config/env.js depuis env.v3.js

--- backend/config/env.js
+++ backend/config/env.js
@@ -8,6 +8,10 @@
 
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
 
-const envPath = path.resolve(process.cwd(), ".env");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const envPath = path.resolve(__dirname, "../../.env");
+
 dotenv.config({ path: envPath });
