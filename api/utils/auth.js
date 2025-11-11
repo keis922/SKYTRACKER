4530170b7c2d4ece40aae02f37eaabf88f773754
keis922 <aissaouikeis@gmail.com>
2025-11-11 11:51:31 +0100

    Version finale de api/utils/auth.js

--- api/utils/auth.js
+++ api/utils/auth.js
@@ -1,26 +1,18 @@
-// Développement : Keïs (structure initiale, intégration Supabase)
-// Révision : Tristan (optimisations et refactorisation visuelle)
-// 
-// • Keïs : logique backend, API, intégration Supabase, structure du projet.
-// • Tristan : front-end, interface graphique, optimisation du rendu, Tailwind, Three.js.
-// 
-// ⸻
-
 import crypto from "crypto";
 
-const ITERATIONS = 100000;
-const KEYLEN = 64;
-
+// keis: hash fort avec pbkdf2
 export function hashPassword(password, salt = crypto.randomBytes(16).toString("hex")) {
-  const hash = crypto.pbkdf2Sync(password, salt, ITERATIONS, KEYLEN, "sha512").toString("hex");
+  const hash = crypto.pbkdf2Sync(password, salt, 100000, 64, "sha512").toString("hex");
   return { salt, hash };
 }
 
+// keis: compare hash simple
 export function comparePassword(password, salt, hash) {
-  const next = crypto.pbkdf2Sync(password, salt, ITERATIONS, KEYLEN, "sha512").toString("hex");
-  return next === hash;
+  const newHash = crypto.pbkdf2Sync(password, salt, 100000, 64, "sha512").toString("hex");
+  return newHash === hash;
 }
 
+// keis: token random long
 export function generateToken() {
   return crypto.randomBytes(32).toString("hex");
 }
